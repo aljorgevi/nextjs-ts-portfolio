16283323f3ff3fb0a111223aa67a34ea88f10c62
aljorgevi <jormencar@yahoo.com>
2023-06-15 22:07:27 +0100

    working on navbar

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,28 +1,50 @@
-import { FC } from 'react'
+'use client'
+
+import { FC, useState } from 'react'
 import { Download } from './Icons/Download'
 
 interface TopNavbarProps {}
 
 const TopNavbar: FC<TopNavbarProps> = ({}) => {
+	const handleDownload = () => {
+		const link = document.createElement('a')
+		link.href = '/CV.pdf' // Replace with the actual path to your CV file
+		link.download = 'Jorge_Mena_CV.pdf' // Replace with the desired file name
+		link.click()
+	}
+
 	return (
 		<>
-			<nav className='p-2 text-[#FFFFFF]'>
-				<div className='flex items-center gap-6'>
+			<nav className='p-4 text-[#FFFFFF]'>
+				<ul className='flex items-center gap-6'>
 					{/* logo */}
-					<div className='text-3xl font-extrabold uppercase '>JM</div>
+					<li className='text-3xl font-extrabold uppercase mr-4'>JM</li>
 
 					{/* CV */}
-
-					<div className='flex gap-2 items-center text-sm'>
-						<Download />
-						<div>CV</div>
-					</div>
+					<li>
+						<a
+							onClick={handleDownload}
+							className='flex gap-2 items-center  relative group hover:opacity-75
+						transition-all duration-300 ease-in-out
+						'
+							href='#'
+						>
+							<Download />
+							<div>CV</div>
+							<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[150%]'></span>
+						</a>
+					</li>
 
 					{/* contact */}
-					<div className='text-sm'>
-						<p>CONTACT</p>
-					</div>
-				</div>
+					<li
+						className='links contact-li cursor-pointer uppercase  shadow py-1 group hover:opacity-75
+						transition-all duration-300 ease-in-out'
+					>
+						<a className='contact-link relative'>
+							<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[120%]'></span>
+						</a>
+					</li>
+				</ul>
 			</nav>
 			<nav className='flex fixed bottom-0 text-[#FFFFFF]'>
 				<ul>
