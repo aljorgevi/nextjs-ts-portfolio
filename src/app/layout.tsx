9faaf0c57ace259999bce2fb79896ace66fb1fd8
aljorgevi <jormencar@yahoo.com>
2023-06-16 23:05:29 +0100

    add styles to main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang='en'>
 			<body className={inter.className}>
 				<TopNavbar />
-				<main>{children}</main>
+				<main className='pl-[4rem]'> {children}</main>
 			</body>
 		</html>
 	)
