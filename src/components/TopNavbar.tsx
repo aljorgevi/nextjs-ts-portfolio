4b77659dd3265bd120a12dc025bdda34af9630ee
aljorgevi <jormencar@yahoo.com>
2023-06-16 23:06:08 +0100

    finish with navbar

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -17,13 +17,15 @@ const TopNavbar: FC<TopNavbarProps> = () => {
 
 	return (
 		<>
-			<nav className='p-4 text-[#FFFFFF]'>
+			{/* <nav className='p-4 '> */}
+			{/* <nav className='[&>ul>li]:p-4'> */}
+			<nav className=''>
 				<ul className='flex items-center '>
 					{/* logo */}
-					<li className='text-3xl font-extrabold uppercase mr-4'>JM</li>
+					<li className='text-3xl font-extrabold uppercase w-[5rem] p-4 pr-0'>JM</li>
 
 					{/* CV */}
-					<li className='shadow-3xl py-3 px-7 ml-4'>
+					<li className='shadow-3xl py-3 px-7 p-4'>
 						<a
 							onClick={handleDownload}
 							className='flex gap-2 items-center  relative group hover:opacity-75
@@ -33,14 +35,14 @@ const TopNavbar: FC<TopNavbarProps> = () => {
 						>
 							<Download />
 							<div>CV</div>
-							<span className='absolute  bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[220%]'></span>
+							<span className='absolute  bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[180%]'></span>
 						</a>
 					</li>
 
 					{/* contact */}
 					<li
 						className='links contact-li cursor-pointer uppercase shadow-3xl py-3 px-3 group hover:opacity-75
-						transition-all duration-300 ease-in-out'
+						transition-all duration-300 ease-in-out p-4'
 					>
 						<a className='contact-link relative'>
 							<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[120%]'></span>
@@ -48,7 +50,13 @@ const TopNavbar: FC<TopNavbarProps> = () => {
 					</li>
 				</ul>
 			</nav>
-			<nav className='fixed bottom-0 text-[#FFFFFF]'>
+
+			{/* [&>li>a]:transition-colors */}
+			<nav
+				className='fixed bottom-0 text-[#FFFFFF]
+				h-[100%] w-[5rem] flex flex-col items-center justify-end
+			'
+			>
 				<ul className='flex flex-col'>
 					<li className='shadow-3xl py-7'>
 						<a href='#'>
