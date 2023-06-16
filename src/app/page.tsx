331ddb8013c96e60b3e943c44b01e2ea36b952fc
aljorgevi <jormencar@yahoo.com>
2023-06-16 23:05:45 +0100

    add Hero and Portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Hero from '@/components/Hero'
+import { Portfolio } from '@/components/Portfolio'
 
 export default function Home() {
 	return (
-		<section className='flex min-h-screen flex-col items-center justify-between p-24'>
-			Jorge Mena, Software Engineer
+		<section className='flex min-h-screen flex-col'>
+			<Hero />
+			<Portfolio />
 		</section>
 	)
 }
