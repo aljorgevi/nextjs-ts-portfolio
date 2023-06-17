c7e10b2ce0be343f764da4f335e040ea5c308d06
aljorgevi <jormencar@yahoo.com>
2023-06-17 22:18:40 +0100

    add animations

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,25 +1,46 @@
+'use client'
+
 import Image from 'next/image'
+
+const projects = [
+	{
+		title: 'Sphera - Beskpoke Services',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+		image: '/sphera.png',
+		linkLive: 'https://sphera.vercel.app/'
+	},
+	{
+		title: 'Website for a gym',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+		image: '/evo.png',
+		linkLive: 'https://evogym-ts.netlify.app/'
+	},
+	{
+		title: 'Loki - rent a car',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+		image: '/loki.png',
+		linkLive: 'https://lokicars.cl/'
+	},
+	{
+		title: 'Rustic Homestead - Airbnb Cabins',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+		image: '/rustic.png',
+		linkLive: 'https://rustic-homestead-chile.netlify.app/'
+	}
+]
 
 export function Portfolio() {
 	return (
 		<section className='grid grid-cols-1 lg:grid-cols-2 mt-10 md:mt-0 gap-16 md:pr-16 md:p-10'>
-			<Project
-				image='/evogym.png'
-				title='Website for a gym'
-				linkLive='https://evogym-ts.netlify.app/'
-			/>
-			<Project
-				image='/evogym.png'
-				title='Website for a gym'
-				linkLive='https://jorge-secondary-portfolio.netlify.app/'
-			/>
-			<Project
-				image='/evogym.png'
-				title='Website for a gym'
-				linkRepo='https://rustic-homestead-chile.netlify.app/'
-				linkLive='asas'
-			/>
-			<Project image='/evogym.png' title='Website for a gym' linkRepo='asa' linkLive='asas' />
+			{projects.map((project, i) => (
+				<Project
+					key={i}
+					image={project.image}
+					title={project.title}
+					linkLive={project.linkLive}
+					description={project.description}
+				/>
+			))}
 		</section>
 	)
 }
@@ -39,16 +60,16 @@ const Project = ({
 }) => {
 	return (
 		<div className='flex flex-col gap-6 items-start'>
-			<div className='mb-0 shadow relative overflow-hidden'>
+			<div className='mb-0 shadow relative overflow-hidden w-[100%]'>
 				<Image
-					// fill
-					height={500}
-					width={500}
-					style={{ objectFit: 'cover' }}
 					src={image}
 					quality={100}
 					className='pointer-events-none select-none'
 					alt='Picture of the author'
+					width={0}
+					height={0}
+					sizes='100vw'
+					style={{ width: '100%', height: 'auto' }} // optional
 				/>
 			</div>
 			<div>
