01457486611bb35c635809f150d463137a1f1aa7
aljorgevi <jormencar@yahoo.com>
2023-06-17 22:18:43 +0100

    add animations

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -5,6 +5,8 @@ import { Download } from './Icons/Download'
 import { GithubIcon } from './Icons/Github'
 import { LinkedinIcon } from './Icons/Linkedin'
 import { Burger } from './Icons/Burger'
+import { AnimatePresence, motion as m } from 'framer-motion'
+import { navbarAnimation } from '@/utils/animations'
 
 interface TopNavbarProps {}
 
@@ -23,60 +25,101 @@ const TopNavbar: FC<TopNavbarProps> = () => {
 	}
 
 	return (
-		<>
-			<div>
-				<div
-					className='hidden
-					md:block
-				'
+		<div>
+			<DesktopNavbar handleDownload={handleDownload} />
+			<MobileNavbar
+				showSidebarHandler={showSidebarHandler}
+				show={show}
+				handleDownload={handleDownload}
+			/>
+			<Sidebar />
+		</div>
+	)
+}
+
+const Sidebar = () => (
+	<nav
+		className='hidden fixed bottom-0 text-[#FFFFFF]
+										h-[100%] w-[5rem] md:flex flex-col items-center justify-end'
+	>
+		<ul className='flex flex-col'>
+			<li className='shadow-3xl py-7'>
+				<a href='#'>
+					<LinkedinIcon width={60} height={25} />
+				</a>
+			</li>
+			<li className='shadow-3xl py-4'>
+				<a href='#'>
+					<GithubIcon className='mb-5' width={60} height={25} />
+				</a>
+			</li>
+		</ul>
+	</nav>
+)
+
+const DesktopNavbar: FC<{ handleDownload: () => void }> = ({ handleDownload }) => (
+	<div className='hidden md:block'>
+		<ul className='flex items-center '>
+			{/* logo */}
+			<li className='text-3xl font-extrabold uppercase w-[5rem] p-4 pr-0'>JM</li>
+
+			{/* CV */}
+			<li className='shadow-3xl py-3 px-7 p-4'>
+				<button
+					onClick={handleDownload}
+					className='flex gap-2 items-center  relative group hover:opacity-75 transition-all duration-300 ease-in-out'
 				>
-					<ul className='flex items-center '>
-						{/* logo */}
-						<li className='text-3xl font-extrabold uppercase w-[5rem] p-4 pr-0'>JM</li>
+					<Download />
+					<div>CV</div>
+					<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[180%]'></span>
+				</button>
+			</li>
 
-						{/* CV */}
-						<li className='shadow-3xl py-3 px-7 p-4'>
-							<button
-								onClick={handleDownload}
-								className='flex gap-2 items-center  relative group hover:opacity-75 transition-all duration-300 ease-in-out'
-							>
-								<Download />
-								<div>CV</div>
-								<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[180%]'></span>
-							</button>
-						</li>
+			{/* contact */}
+			<li className='links contact-li cursor-pointer uppercase shadow-3xl py-3 px-3 group hover:opacity-75 transition-all duration-300 ease-in-out p-4'>
+				<button className='contact-link relative'>
+					<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[120%]'></span>
+				</button>
+			</li>
+		</ul>
+	</div>
+)
 
-						{/* contact */}
-						<li className='links contact-li cursor-pointer uppercase shadow-3xl py-3 px-3 group hover:opacity-75 transition-all duration-300 ease-in-out p-4'>
-							<button className='contact-link relative'>
-								<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[120%]'></span>
-							</button>
-						</li>
-					</ul>
-				</div>
+const MobileNavbar: FC<{
+	showSidebarHandler: () => void
+	show: boolean
+	handleDownload: () => void
+}> = ({ showSidebarHandler, show, handleDownload }) => (
+	<nav>
+		<div className='md:hidden flex justify-between items-center px-6 py-3'>
+			<ul className='flex'>
+				<li className='shadow-3xl '>
+					<a href='#'>
+						<LinkedinIcon width={60} height={25} />
+					</a>
+				</li>
+				<li className='shadow-3xl'>
+					<a href='#'>
+						<GithubIcon className='' width={60} height={25} />
+					</a>
+				</li>
+			</ul>
 
-				<div className='md:hidden flex justify-end pr-5 pt-1'>
-					<div>
-						<div className='flex justify-between items-center h-6rem'>
-							<button className='p-1.5 cursor-pointer' onClick={showSidebarHandler}>
-								<Burger />
-							</button>
-						</div>
-					</div>
-				</div>
+			<button className='cursor-pointer' onClick={showSidebarHandler}>
+				<Burger />
+			</button>
+		</div>
 
-				<div className='w-screen'>
-					<div
-						className={`flex flex-col justify-center items-center transition ${
-							show ? 'h-38rem' : 'h-0 overflow-hidden'
-						}`}
-					>
+		<AnimatePresence mode='wait' initial={false}>
+			{show && (
+				<m.div key='navbar-details' className='h-[70px] w-screen my-4' {...navbarAnimation}>
+					<div className='flex flex-col justify-center items-center transition h-38rem'>
 						<ul>
 							<li className='flex justify-center'>
 								<button
 									onClick={handleDownload}
 									className='flex gap-2 items-center group hover:opacity-75 mb-2
-									transition-all duration-300 ease-in-out relative'
+						transition-all duration-300 ease-in-out relative'
 								>
 									<Download />
 									<div>CV </div>
@@ -91,29 +134,10 @@ const TopNavbar: FC<TopNavbarProps> = () => {
 							</li>
 						</ul>
 					</div>
-				</div>
-
-				<nav
-					className='hidden fixed bottom-0 text-[#FFFFFF]
-		h-[100%] w-[5rem] md:flex flex-col items-center justify-end
-	'
-				>
-					<ul className='flex flex-col'>
-						<li className='shadow-3xl py-7'>
-							<a href='#'>
-								<LinkedinIcon width={60} height={25} />
-							</a>
-						</li>
-						<li className='shadow-3xl py-4'>
-							<a href='#'>
-								<GithubIcon className='mb-5' width={60} height={25} />
-							</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</>
-	)
-}
+				</m.div>
+			)}
+		</AnimatePresence>
+	</nav>
+)
 
 export default TopNavbar
