dcfcb11a3f546297c6a645cea1b2b654416b0000
aljorgevi <jormencar@yahoo.com>
2023-06-17 22:18:46 +0100

    add animations

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,20 +1,52 @@
-export const contentAnimation = {
+export const heroAnimation = {
 	initial: 'hidden',
-	whileInView: 'show',
+	animate: 'show',
 	exit: 'exit',
-	transition: { duration: 0.5, ease: 'easeInOut' },
+	transition: { duration: 0.5, ease: 'easeInOut', delay: 0.2 },
 	variants: {
 		hidden: {
+			y: 0,
 			opacity: 0,
-			y: -50
+			height: 0,
+			overflow: 'hidden'
 		},
 		show: {
+			y: 0,
 			opacity: 1,
-			y: 10
+			transition: { duration: 1 },
+			height: 'auto',
+			overflow: 'hidden'
 		},
 		exit: {
+			y: 0,
 			opacity: 0,
-			y: -50
+			transition: { duration: 0.7 },
+			height: 0,
+			overflow: 'hidden'
+		}
+	}
+}
+
+export const navbarAnimation = {
+	initial: 'hidden',
+	animate: 'show',
+	exit: 'exit',
+	transition: { duration: 0.5, ease: 'easeOut', delay: 0.5 },
+	variants: {
+		hidden: {
+			y: 0,
+			opacity: 0,
+			height: 0
+		},
+		show: {
+			y: 0,
+			opacity: 1,
+			height: 'auto'
+		},
+		exit: {
+			y: 0,
+			opacity: 0,
+			height: 0
 		}
 	}
 }
