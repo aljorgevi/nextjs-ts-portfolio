4a677da8d591baea9bc3c3fa119b7f29d7853ac4
aljorgevi <jormencar@yahoo.com>
2023-06-17 17:26:57 +0100

    remove useMediaQuery

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -4,13 +4,11 @@ import { FC, useState } from 'react'
 import { Download } from './Icons/Download'
 import { GithubIcon } from './Icons/Github'
 import { LinkedinIcon } from './Icons/Linkedin'
-import { useMediaQuery } from '@/app/hooks/useMediaQuery'
 import { Burger } from './Icons/Burger'
 
 interface TopNavbarProps {}
 
 const TopNavbar: FC<TopNavbarProps> = () => {
-	const desktop = useMediaQuery('(min-width: 768px)')
 	const [show, setShow] = useState(false)
 
 	const showSidebarHandler = () => {
@@ -27,77 +25,73 @@ const TopNavbar: FC<TopNavbarProps> = () => {
 	return (
 		<>
 			<div>
-				{desktop ? (
-					<div className=''>
-						<ul className='flex items-center '>
-							{/* logo */}
-							<li className='text-3xl font-extrabold uppercase w-[5rem] p-4 pr-0'>JM</li>
+				<div
+					className='hidden
+					md:block
+				'
+				>
+					<ul className='flex items-center '>
+						{/* logo */}
+						<li className='text-3xl font-extrabold uppercase w-[5rem] p-4 pr-0'>JM</li>
 
-							{/* CV */}
-							<li className='shadow-3xl py-3 px-7 p-4'>
+						{/* CV */}
+						<li className='shadow-3xl py-3 px-7 p-4'>
+							<button
+								onClick={handleDownload}
+								className='flex gap-2 items-center  relative group hover:opacity-75 transition-all duration-300 ease-in-out'
+							>
+								<Download />
+								<div>CV</div>
+								<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[180%]'></span>
+							</button>
+						</li>
+
+						{/* contact */}
+						<li className='links contact-li cursor-pointer uppercase shadow-3xl py-3 px-3 group hover:opacity-75 transition-all duration-300 ease-in-out p-4'>
+							<button className='contact-link relative'>
+								<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[120%]'></span>
+							</button>
+						</li>
+					</ul>
+				</div>
+
+				<div className='md:hidden flex justify-end pr-5 pt-1'>
+					<div>
+						<div className='flex justify-between items-center h-6rem'>
+							<button className='p-1.5 cursor-pointer' onClick={showSidebarHandler}>
+								<Burger />
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<div className='w-screen'>
+					<div
+						className={`flex flex-col justify-center items-center transition ${
+							show ? 'h-38rem' : 'h-0 overflow-hidden'
+						}`}
+					>
+						<ul>
+							<li className='flex justify-center'>
 								<button
 									onClick={handleDownload}
-									className='flex gap-2 items-center  relative group hover:opacity-75
-transition-all duration-300 ease-in-out
-'
+									className='flex gap-2 items-center group hover:opacity-75 mb-2
+									transition-all duration-300 ease-in-out relative'
 								>
 									<Download />
-									<div>CV</div>
-									<span className='absolute  bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[180%]'></span>
+									<div>CV </div>
+									<span className='absolute  bg-[#fff] h-[3px] w-[85vw] bottom-[-15px] left-1/2 -translate-x-1/2'></span>
 								</button>
 							</li>
-
-							{/* contact */}
-							<li
-								className='links contact-li cursor-pointer uppercase shadow-3xl py-3 px-3 group hover:opacity-75
-transition-all duration-300 ease-in-out p-4'
-							>
-								<button className='contact-link relative'>
-									<span className='absolute bg-white h-[3px] w-0 bottom-[-15px] left-1/2 transition-all duration-300 origin-center transform-gpu -translate-x-1/2 group-hover:w-[120%]'></span>
-								</button>
+							<li>
+								<div className='relative pt-3'>
+									<div>jormencar@gmail.com </div>
+									<span className='absolute  bg-[#fff] h-[3px] w-[85vw] bottom-[-15px] left-1/2 -translate-x-1/2'></span>
+								</div>
 							</li>
 						</ul>
 					</div>
-				) : (
-					<>
-						<div className='flex items-center justify-end pr-5 pt-1'>
-							<div>
-								<div className='flex justify-between items-center h-6rem'>
-									<button className='p-1.5' onClick={showSidebarHandler}>
-										<Burger />
-									</button>
-								</div>
-							</div>
-						</div>
-						<div className='w-screen'>
-							<div
-								className={`flex flex-col justify-center items-center transition ${
-									show ? 'h-38rem' : 'h-0 overflow-hidden'
-								}`}
-							>
-								<ul>
-									<li className='flex justify-center'>
-										<button
-											onClick={handleDownload}
-											className='flex gap-2 items-center group hover:opacity-75 mb-2
-									transition-all duration-300 ease-in-out relative'
-										>
-											<Download />
-											<div>CV </div>
-											<span className='absolute  bg-[#fff] h-[3px] w-[85vw] bottom-[-15px] left-1/2 -translate-x-1/2'></span>
-										</button>
-									</li>
-									<li>
-										<div className='relative pt-3'>
-											<div>jormencar@gmail.com </div>
-											<span className='absolute  bg-[#fff] h-[3px] w-[85vw] bottom-[-15px] left-1/2 -translate-x-1/2'></span>
-										</div>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</>
-				)}
+				</div>
 
 				<nav
 					className='hidden fixed bottom-0 text-[#FFFFFF]
