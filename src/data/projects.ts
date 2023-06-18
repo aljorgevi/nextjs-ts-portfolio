acfa1da4a96dd73af770b31b4ca2b54a2e680fa8
aljorgevi <jormencar@yahoo.com>
2023-06-18 21:58:48 +0100

    move projects

---
+++ src/data/projects.ts
@@ -0,0 +1,60 @@
+import { Project } from '@/types/types'
+
+export const projects: Project[] = [
+	{
+		title: 'Sphera',
+		description: `Bespoke Services company with elegant design, showcasing exceptional tailored services. Carefully crafted for seamless user experience, enabling easy navigation and access to relevant information.`,
+		image: '/sphera.png',
+		linkLive: 'https://sphera.vercel.app/',
+		linkRepo: '',
+		type: 'web'
+	},
+	{
+		title: 'Evo Gym',
+		description: `Dynamic website for a local gym showcasing state-of-the-art equipment, diverse fitness classes, and a passionate community. Modern design promotes an active lifestyle, with intuitive user experience for easy navigation and access to information.`,
+		image: '/evo.png',
+		linkLive: 'https://evogym-ts.netlify.app/',
+		linkRepo: '',
+		type: 'web'
+	},
+	{
+		title: 'Loki',
+		description: `Spanish rent-a-car company in Chile. A user-friendly website, offering effortless and reliable vehicle rentals. Explore vehicles, compare prices, and make reservations seamlessly. Experience exceptional service and attention to detail through our sleek and intuitive design.`,
+		image: '/loki.png',
+		linkLive: 'https://lokicars.cl/',
+		linkRepo: '',
+		type: 'web'
+	},
+	{
+		title: 'Rustic Homestead - Airbnb Cabins',
+		description: `Website for a rustic Airbnb cabin, meticulously crafted to showcase its unique features. Engage with a seamless reservation form, explore captivating images of the cabin, and immerse yourself in its rustic charm. The website offers an intuitive user experience, inviting visitors to embark on an unforgettable journey of discovery and relaxation.`,
+		image: '/rustic.png',
+		linkLive: 'https://rustic-homestead-chile.netlify.app/',
+		linkRepo: '',
+		type: 'web'
+	},
+	{
+		title: 'Portfolio',
+		description: `Personal portfolio website, this portfolio helped me to get my first job as web developer. The website offers a seamless user experience, with intuitive navigation and access to relevant information. The design is modern and elegant, with a minimalist approach to highlight the content.`,
+		image: '/aljorgevi.png',
+		linkLive: 'https://jorge-secondary-portfolio.netlify.app/',
+		linkRepo: '',
+		type: 'web'
+	},
+	{
+		title: 'Ecommerce Furniture',
+		description: `Furniture Fantastic, Ecommerce website for a furniture company, offering a wide range of products. The website offers a seamless user experience, with intuitive navigation and access to relevant information. The design is modern and elegant, with a minimalist approach to highlight the content.`,
+		image: '/furniture-fantastic.png',
+		linkLive: 'https://furniture-fantastic.netlify.app/',
+		linkRepo: '',
+		type: 'web'
+	},
+	{
+		title: 'Unsplash Gallery',
+		description: `Unsplash Gallery, a website to search for images from Unsplash API. Crated with React, this website offerts infinite scroll and search by keyword.`,
+		image: '/unsplash-gallery.png',
+		linkLive: 'https://stock-photos-unsplash.netlify.app/',
+		linkRepo: 'https://github.com/aljorgevi/Stock-photos-API',
+		type: 'project'
+	}
+]
