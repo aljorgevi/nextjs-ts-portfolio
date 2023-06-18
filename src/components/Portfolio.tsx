ee7cfd62a3bd2ba27258ccd2f94de69c70ad91e1
aljorgevi <jormencar@yahoo.com>
2023-06-18 21:58:35 +0100

    refactor projects., add types, move to anothes files

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,86 +2,50 @@
 
 import Image from 'next/image'
 import { motion as m } from 'framer-motion'
-
-const projects = [
-	{
-		title: 'Sphera',
-		description: `Bespoke Services company with elegant design, showcasing exceptional tailored services. Carefully crafted for seamless user experience, enabling easy navigation and access to relevant information.`,
-		image: '/sphera.png',
-		linkLive: 'https://sphera.vercel.app/'
-	},
-	{
-		title: 'Evo Gym',
-		description: `Dynamic website for a local gym showcasing state-of-the-art equipment, diverse fitness classes, and a passionate community. Modern design promotes an active lifestyle, with intuitive user experience for easy navigation and access to information.`,
-		image: '/evo.png',
-		linkLive: 'https://evogym-ts.netlify.app/'
-	},
-	{
-		title: 'Loki',
-		description: `Spanish rent-a-car company in Chile. A user-friendly website, offering effortless and reliable vehicle rentals. Explore vehicles, compare prices, and make reservations seamlessly. Experience exceptional service and attention to detail through our sleek and intuitive design.`,
-		image: '/loki.png',
-		linkLive: 'https://lokicars.cl/'
-	},
-	{
-		title: 'Rustic Homestead - Airbnb Cabins',
-		description: `Website for a rustic Airbnb cabin, meticulously crafted to showcase its unique features. Engage with a seamless reservation form, explore captivating images of the cabin, and immerse yourself in its rustic charm. The website offers an intuitive user experience, inviting visitors to embark on an unforgettable journey of discovery and relaxation.`,
-		image: '/rustic.png',
-		linkLive: 'https://rustic-homestead-chile.netlify.app/'
-	},
-	{
-		title: 'Portfolio',
-		description: `Personal portfolio website, this portfolio helped me to get my first job as web developer. The website offers a seamless user experience, with intuitive navigation and access to relevant information. The design is modern and elegant, with a minimalist approach to highlight the content.`,
-		image: '/aljorgevi.png',
-		linkLive: 'https://jorge-secondary-portfolio.netlify.app/'
-	},
-	{
-		title: 'Ecommerce Furniture',
-		description: `Furniture Fantastic, Ecommerce website for a furniture company, offering a wide range of products. The website offers a seamless user experience, with intuitive navigation and access to relevant information. The design is modern and elegant, with a minimalist approach to highlight the content.`,
-		image: '/furniture-fantastic.png',
-		linkLive: 'https://furniture-fantastic.netlify.app/'
-	}
-]
+import { FC, useState } from 'react'
+import { Project, ProjectType } from '@/types/types'
+import { projects } from '@/data/projects'
 
 export function Portfolio() {
+	const [active, setActive] = useState<ProjectType>('web')
+
+	function handleFilterChange(filter: ProjectType) {
+		setActive(filter)
+	}
+
 	return (
-		<m.section
-			initial={{
-				opacity: 0
-			}}
-			animate={{
-				opacity: 1,
-				transition: {
-					duration: 0.5,
-					delay: 0.9
-				}
-			}}
-			className='grid grid-cols-1 lg:grid-cols-2 mt-10 md:mt-0 gap-16 md:pr-16 md:p-10'
-		>
-			{projects.map((project, i) => (
-				<Project
-					key={i}
-					image={project.image}
-					title={project.title}
-					linkLive={project.linkLive}
-					description={project.description}
-				/>
-			))}
-		</m.section>
+		<section>
+			<CardFilter onClick={handleFilterChange} active={active} />
+			<m.section
+				initial={{
+					opacity: 0
+				}}
+				animate={{
+					opacity: 1,
+					transition: {
+						duration: 0.5,
+						delay: 0.9
+					}
+				}}
+				className='grid grid-cols-1 lg:grid-cols-2 mt-10 md:mt-0 gap-16 md:pr-16 md:p-10'
+			>
+				{projects
+					.filter(project => project.type === active || active === 'all')
+					.map((project, index) => (
+						<ProjectCard key={index} {...project} />
+					))}
+			</m.section>
+		</section>
 	)
 }
 
-const Project = ({
+const ProjectCard: FC<Project> = ({
 	title,
 	description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Quisquam, voluptatum.',
 	image,
 	linkLive,
-	linkRepo
-}: {
-	title: string
-	description?: string
-	image: string
-	linkLive: string
-	linkRepo?: string
+	linkRepo,
+	type
 }) => {
 	return (
 		<div className='flex flex-col gap-6 items-start'>
@@ -123,7 +87,7 @@ const Project = ({
 					>
 						Live
 					</a>
-					{linkRepo && (
+					{Boolean(linkRepo) && (
 						<a
 							href={linkRepo}
 							className='hover:opacity-70 transition duration-200 ease-in-out'
@@ -138,5 +102,58 @@ const Project = ({
 				<p className='max-w-lg'>{description}</p>
 			</div>
 		</div>
+	)
+}
+
+const CardFilter = ({
+	onClick,
+	active
+}: {
+	onClick: (filter: ProjectType) => void
+	active: ProjectType
+}) => {
+	return (
+		<m.div
+			initial={{
+				opacity: 0
+			}}
+			animate={{
+				opacity: 1,
+				transition: {
+					duration: 0.5,
+					delay: 0.9
+				}
+			}}
+			className='uppercase mt-4 max-w-[14rem] md:max-w-none md:pl-[2.25rem] inline-block'
+		>
+			<div className='flex overflow-hidden  border border-[#fff] divide-x  rtl:flex-row-reverse bg-[#191919]  divide-[#fff]'>
+				<button
+					onClick={() => onClick('web')}
+					className={`px-4 py-2 text-sm font-medium transition-colors duration-200 sm:text-base sm:px-6  text-gray-300 hover:bg-[#fff] hover:text-[#191919]
+					${active === 'web' && 'bg-[#fff] text-[#191919]'}
+					`}
+				>
+					Web
+				</button>
+
+				<button
+					onClick={() => onClick('project')}
+					className={`px-4 py-2 text-sm font-medium  transition-colors duration-200 sm:text-base sm:px-6  text-gray-300 hover:bg-[#fff] hover:text-[#191919]
+					${active === 'project' && 'bg-[#fff] text-[#191919]'}
+					`}
+				>
+					Projects
+				</button>
+
+				<button
+					onClick={() => onClick('all')}
+					className={`px-4 py-2 text-sm font-medium  transition-colors duration-200 sm:text-base sm:px-6  text-gray-300 hover:bg-[#fff] hover:text-[#191919]
+					${active === 'all' && 'bg-[#fff] text-[#191919]'}
+					`}
+				>
+					All
+				</button>
+			</div>
+		</m.div>
 	)
 }
